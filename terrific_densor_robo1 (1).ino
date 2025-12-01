56cfcb54a701723cddd7b6ac7c05fb6f679d20b9
Hanslmnop <thisspata@gmail.com>
2025-12-01 14:02:08 +1000

    traffic light (simple)

--- terrific_densor_robo1 (1).ino
+++ terrific_densor_robo1 (1).ino
@@ -3,6 +3,8 @@
 void setup()
 {
   pinMode(2, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(0, OUTPUT);
 }
 
 void loop()
@@ -10,5 +12,15 @@ void loop()
   digitalWrite(2, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+
+  digitalWrite(1, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(1, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+
+  digitalWrite(0, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(0, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
 }
